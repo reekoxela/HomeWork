373f616ac4df09622030e0f69e873532213bd786
Alexander <reekoxela@yandex.ru>
2022-11-23 17:12:41 +0300

    Fourth Homework C#

--- Program.cs
+++ Program.cs
@@ -1,75 +1,66 @@
 ﻿/*
-//программа принимает на вход пятизначное число и проверяет, является ли оно палиндромом
-//первый вариант
-Console.Write("Введите пятизначное число: ");
-string number = Console.ReadLine()!;
- if (number.Length != 5) {
-    Console.WriteLine("Ошибка! Это не пятизначное число!");
-}
-else {
-    int length = number.Length;
-    bool flag = true;
-    for(int i = 0; i < length / 2; i++) {
-        if (number[i] != number[length - i - 1])
-        {
-             flag = false;
-             break;
+//программа принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень числа: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {A} в степени {B} равно: {GetDegree(A,B)}");
+
+//-----------------------------------------------МЕТОД---------------------------------------------------
+int GetDegree(int number, int degree) {
+    if(degree == 0) {
+        return 1;
+    }
+    else {
+        int result = number;
+        for(int i = 1; i < degree; i++) {
+            result *= number;
         }
+        return result;
     }
-    if (flag == true) {
-        Console.WriteLine("Число является палиндромом");
-    }
-    else {
-        Console.WriteLine("Число не является палиндромом");
-    }
+}
 
-}    
-//второй вариант
-Console.Write("Введите пятизначное число: ");
+
+//программу принимает на вход число и выдаёт сумму цифр в числе
+Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
-if (number < 10000 || number > 99999) {
-    Console.WriteLine("Ошибка! Это не пятизначное число!");
-}
-else {
-    int number2 = 0, n1, n2 = number;
-    while (n2 > 0) {
-        n1 = n2 % 10;
-        n2 = n2 / 10;
-        number2 = number2 * 10 + n1;
-    }
-    if (number == number2)  {
-        Console.WriteLine("Число является палиндромом");
-    }
-    else {
-        Console.WriteLine("Число не является палиндромом");
-    }
-}
+Console.WriteLine($"Сумма цифр в числе {number} равна {GetSum(number)} ");
 
-//программа принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
-Console.WriteLine("Введите координаты первой точки: ");
-Console.Write("Введите Xa =  ");
-int Xa = int.Parse(Console.ReadLine()!);
-Console.Write("Введите Ya = ");
-int Ya = int.Parse(Console.ReadLine()!);
-Console.Write("Введите Za = ");
-int Za = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите координаты второй точки: ");
-Console.Write("Введите Xb =  ");
-int Xb = int.Parse(Console.ReadLine()!);
-Console.Write("Введите Yb = ");
-int Yb = int.Parse(Console.ReadLine()!);
-Console.Write("Введите Zb = ");
-int Zb = int.Parse(Console.ReadLine()!);
-
-double result = Math.Pow(Xb - Xa, 2) + Math.Pow(Yb - Ya, 2) + Math.Pow(Zb - Za, 2);
-Console.WriteLine($"Расстояние между точками: {Math.Sqrt(result):f2}");
+//-----------------------------------------------МЕТОД---------------------------------------------------
+int GetSum(int num) {
+    int result = 0;
+    while(num > 0) {
+        result += num % 10;
+        num /= 10;
+     }
+     return result;
+}
 */
 
-//программа принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
-Console.Write("Введите число =  ");
-int N = int.Parse(Console.ReadLine()!);
-
-Console.Write($"Таблица кубов чисел от 1 до {N}: ");
-for(int i = 1; i <= N; i++) {
-    Console.Write($"{Math.Pow(i,3)} ");
+// программа задаёт массив из 8 элементов и выводит их на экран
+const int size = 8;
+int[] mass = InitArray(size);
+Console.WriteLine($"Массив из {size} элементов: [{String.Join(",",mass)}]");
+Console.Write($"Массив из {size} элементов: ");
+PrintArray(mass);
+//-----------------------------------------------МЕТОДЫ--------------------------------------------------
+//Заполнение массива
+int[] InitArray(int size) {
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++) {
+        array[i] = new Random().Next(0,151);
+    }
+    return array;
+}
+//Печать массива
+void PrintArray(int[] array) {
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++) {
+        if (i == array.Length - 1) {
+            Console.Write($"{array[i]}]");    
+        }
+        else {
+            Console.Write($"{array[i]},");
+        }
+    }
 }
