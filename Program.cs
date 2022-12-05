696085d85fa288b4a386f34d6176cde1de2c6d4c
Alexander <reekoxela@yandex.ru>
2022-12-05 14:06:55 +0300

    Seventh homework C#

--- Program.cs
+++ Program.cs
@@ -1,58 +1,119 @@
 ﻿/*
-//ввести с клавиатуры M чисел. Посчитать, сколько чисел строго больше 0 ввёл пользователь
-Console.Write("Введите размер массива: ");
-int M = int.Parse(Console.ReadLine()!);
-int[] mass = InitArray(M);
-Console.WriteLine($"Массив: [{String.Join(",",mass)}]");
-Console.WriteLine($"Количество чисел больше 0: {Larger0(mass)}");
-//-----------------------------------------------МЕТОДЫ--------------------------------------------------
-//Заполнение массива
-int[] InitArray(int size){
-    int[] array = new int[size];
-    for(int i = 0; i < size; i++){
-        Console.Write($"Введите {i + 1} элемент массива: ");
-        array[i] = int.Parse(Console.ReadLine()!);
-    }
-    return array;
-}
+//двумерный массив размером m×n, заполненный случайными вещественными числами
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine()!);
 
-//Количество чисел больше 0
-int Larger0(int[] array){
-    int count = 0;
-    for(int i = 0; i < array.Length; i++){
-        if(array[i] > 0) count++;
-    }
-    return count;
-}
-*/
-
-//программа принимает массив в промежутке от 0 до 100, на выходе выводит этот же массив отсортированный по возрастанию
-Console.Write("Введите размер массива: ");
-int M = int.Parse(Console.ReadLine()!);
-int[] mass = InitArray(M,0,100);
-Console.WriteLine($"Массив: [{String.Join(",",mass)}]");
-SortArray(mass);
-Console.WriteLine($"Отсортированный массив: [{String.Join(",",mass)}]");
-
-//-----------------------------------------------МЕТОДЫ--------------------------------------------------
-//Ввод массива
-int[] InitArray(int size, int minValue, int maxValue){
-    int[] array = new int[size];
-    for(int i = 0; i < size; i++){
-        array[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return array;
-}
-
-//Сортировка массива
-void SortArray(int[] array){
-    for(int i = 1; i < array.Length; i++){
-        for(int j = 0; j < array.Length - i; j++) {
-            if(array[j] > array[j + 1]){
-                int tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp; 
-            }
+double[,] mass = GetArray(m,n,-50,50);
+PrintArray(mass);
+//------------------------------------МЕТОДЫ-------------------------------------------
+//Создания двумерного массива
+double[,] GetArray(int rows, int cols, int minValue, int maxValue){
+    double[,] result = new double[rows, cols];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            result[i,j] = new Random().Next(minValue, maxValue) + new Random().NextDouble();
         }
     }
+    return result;
+}
+
+// Вывод двумерного масссива
+void PrintArray(double[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]:f1}     ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//***************************************************************************************************************************************
+//программа принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+int[,] mass = GetArray(3, 4, 0, 50);
+PrintArray(mass);
+
+Console.Write("Введите номер строки массива: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите номер столбца массива: ");
+int col = int.Parse(Console.ReadLine()!);
+
+if (row < 0 || col < 0){
+    Console.Write("Номера строки и столбца должны быть положительными!");
+}
+else{
+    FindElement(mass,row,col);
+}
+
+//------------------------------------МЕТОДЫ-------------------------------------------
+//Создания двумерного массива
+int[,] GetArray(int rows, int cols, int minValue, int maxValue){
+    int[,] result = new int[rows, cols];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+// Вывод двумерного масссива
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] array, int row, int col){
+    if(row > array.GetLength(0) || col > array.GetLength(1)){
+        Console.WriteLine($"Элемента с позицией [{row},{col}] в массиве нет!");
+    }
+    else{
+        Console.WriteLine($"Элемент с позицией [{row},{col}] = {array[row,col]}");
+    }
+*/
+
+//***************************************************************************************************************************************
+//Найти среднее арифметическое элементов в каждом столбце двумерного массива
+int[,] mass = GetArray(3, 4, 0, 10);
+PrintArray(mass);
+double[] avr = AverageCols(mass);
+Console.WriteLine($"Среднее арифметическое элементов в каждом столбце: [{string.Join("   ",avr)}]");
+//------------------------------------МЕТОДЫ-------------------------------------------
+//Создания двумерного массива
+int[,] GetArray(int rows, int cols, int minValue, int maxValue){
+    int[,] result = new int[rows, cols];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+// Вывод двумерного масссива
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Cреднее арифметическое элементов в каждом столбце двумерного массива
+double[] AverageCols(int[,] array){
+    double[] avr = new double[array.GetLength(1)];
+    for(int j = 0; j < array.GetLength(1); j++){
+        double sum = 0;
+        for(int i = 0; i < array.GetLength(0); i++){
+            sum += array[i,j];
+        }
+        avr[j] = Math.Round(sum / array.GetLength(0),2);
+    }
+    return avr;    
 }
